2c5e43720604c8c38463773a6de246cab293f530
Oliver Spinks <oliver-spinks@hotmail.com>
2022-06-24 18:44:05 +0100

    Finished ProductDetailsPage Demo

---
+++ pages/ProductDetailsPage.js
@@ -0,0 +1,11 @@
+import { Selector } from "testcafe";
+
+class ProductDetailsPage{
+    constructor() {
+        this.productPrice = Selector("span#price-value-4.price-value-4").withText('$1,800.00')
+        this.productQuantity = Selector("input#product_enteredQuantity_4.qty-input")
+        this.addToCart = Selector("button#add-to-cart-button-4.button-1.add-to-cart-button")
+        this.successMessage = Selector('div.bar-notification.success');
+    }
+}
+export default new ProductDetailsPage();
